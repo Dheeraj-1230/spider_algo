c6cba0899aff5c0f4e78702b36bbc135c3931896
Dheeraj-1230 <63278930+Dheeraj-1230@users.noreply.github.com>
2020-08-19 17:01:20 +0530

    updated

--- task1_A.cpp
+++ task1_A.cpp
@@ -5,12 +5,12 @@ int main()
 {
 	int n;
 	cin>>n;
-	char s[n];
+	string st;
+	cin>>st;
+	char s[n],r[n];
 	for(int i=0;i<n;i++)
-	cin>>s[i];
-    char r[n];
-	for(int i=0;i<n;i++)
-	r[i]=s[i];
+	r[i]=s[i]=st[i];
+   
 	int n1=0,n0=0;
 	for(int i=0;i<n;i++)
 	{
@@ -21,28 +21,6 @@ int main()
 	}
 	if(n0==0||n1==0)
 	cout<<-1<<endl;
-	else if(n1==0 &&n0==1)
-	cout<<"11"<<" "<<"01"<<endl;
-	else if(n1==1&&n0!=0)
-	{
-			for(int i=n-1;i>=0;i--)
-				{
-					if(r[i]=='0')
-						{
-							r[i]='1';
-							break;
-						}
-				}
-		   	for(int i=0;i<n;i++)
-				{
-					if(s[i]=='1')
-							s[i]='0';
-					else if(s[i]=='0')
-						s[i]='1';
-				}
-			cout<<r<<" "<<s<<endl;
-		
-	}
 	else
 	{
 		for(int i=n-1;i>=0;i--)
@@ -65,7 +43,12 @@ int main()
 			else if(r[i]=='1')
 			r[i]='0';
 		}
-		cout<<r<<" "<<s<<endl;
+	for(int i=0;i<n;i++)
+	cout<<s[i];
+	cout<<" ";
+	for(int i=0;i<n;i++)
+	cout<<r[i];
+	cout<<endl;
 	}
 
 	return 0;
